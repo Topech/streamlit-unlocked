1808bd8b416efadbf421cb6d8b044a3466e376ca
Topech <benlbttm0@gmail.com>
2026-02-01 00:26:08 +0800

    return rerun strategies

--- src/smart_fragment.py
+++ src/smart_fragment.py
@@ -1,14 +1,10 @@
 import inspect
 from pathlib import Path
-from typing import Callable, ParamSpec, TypeVar
+from typing import Callable, Literal, ParamSpec, TypeAlias, TypeVar, Union
 
 import streamlit as st
 
-from src.auto_key import (
-    auto_key_layer,
-    get_key,
-    key_stack_is_empty,
-)
+from src import auto_key
 
 P = ParamSpec("P")
 R = TypeVar("R")
@@ -16,49 +12,88 @@ R = TypeVar("R")
 # inputs -> go in
 
 
-def smart_fragment(fn: Callable[P, R]) -> Callable[P, R | None]:
-    def _create_fragment_closure(*args, **kwargs):
-        caller_frame = inspect.currentframe().f_back
-        raw_used_in_file = caller_frame.f_code.co_filename
-        used_in_file = Path(raw_used_in_file).relative_to(Path.cwd())
-        used_at_line_no = caller_frame.f_lineno
-
-        raw_declared_in_file = inspect.getmodule(fn).__file__
-        declared_in_file = Path(raw_declared_in_file).relative_to(Path.cwd())
-        declared_at_line_no = fn.__code__.co_firstlineno
-
-        key_layer = f"<{fn.__name__}: {declared_in_file}[{declared_at_line_no}]> @ {used_in_file}[{used_at_line_no}]"
-
-        # NOTE: we get root key layer out of closure to 'persist' it for fragment reruns
-        fragment_root_key_layer = get_key(key_layer)
-
-        @st.fragment
-        def _fragment_closure(*args, **kwargs):
-            # differentiates if the fragment is running in isolation OR as part of whole app re-run
-            if key_stack_is_empty():
-                # TODO: # restore_key_stack_for_smart_fragment(fragment_root_key_layer)
-                fragment_key_layer = fragment_root_key_layer
-            else:
-                fragment_key_layer = key_layer
-
-            with auto_key_layer(fragment_key_layer) as generated_fragment_key:
-                output = fn(*args, **kwargs)
-
-            # this is odd, but the output is returned on re-run, so this is actually getting the output from before the
-            # triggered rerun
-            previous_output = st.session_state.get(generated_fragment_key, None)
-
-            # save the output for next re-run
-            st.session_state[generated_fragment_key] = output
-
-            if output is not None:
-                # todo: some way to better scope reruns?
-                st.rerun(scope="app")
-            return previous_output
-
-        return _fragment_closure(*args, **kwargs)
-
-    return _create_fragment_closure
+SmartFragmentReturnStrategies: TypeAlias = Union[
+    Literal["rerun_not_on_none"], Literal["rerun_on_change"], Literal["no_rerun"]
+]
 
 
-# output -> None, only callbacks?
+def smart_fragment(
+    fn: Callable[P, R] = None,
+    *,
+    on_return_strategy: SmartFragmentReturnStrategies = "rerun_on_not_none",
+) -> Callable[P, R]:
+    def _closed_parameters_decorator(fn):
+        """this layer justs lets us optionally provide parameters"""
+
+        def _frame_intercepting_wrapper(*args, **kwargs):
+            caller_frame = inspect.currentframe().f_back
+            raw_used_in_file = caller_frame.f_code.co_filename
+            used_in_file = Path(raw_used_in_file).relative_to(Path.cwd())
+            used_at_line_no = caller_frame.f_lineno
+
+            raw_declared_in_file = inspect.getmodule(fn).__file__
+            declared_in_file = Path(raw_declared_in_file).relative_to(Path.cwd())
+            declared_at_line_no = fn.__code__.co_firstlineno
+
+            key_layer = f"<{fn.__name__}: {declared_in_file}[{declared_at_line_no}]> @ {used_in_file}[{used_at_line_no}]"
+
+            # NOTE: we get root key layer out of inner_fragment to 'persist' it for fragment reruns
+            fragment_root_key_layer = auto_key.get_key(key_layer)
+
+            @st.fragment
+            def _wrapped_fragment(*args, **kwargs):
+                # differentiates if the fragment is running in isolation OR as part of whole app re-run
+                if auto_key.key_stack_is_empty():
+                    fragment_key_layer = fragment_root_key_layer
+                else:
+                    fragment_key_layer = key_layer
+
+                with auto_key.auto_key_layer(
+                    fragment_key_layer
+                ) as generated_fragment_key:
+                    output = fn(*args, **kwargs)
+
+                fragment_result_key = f"RETURN: {generated_fragment_key}"
+
+                _is_first_time_calling_fragment = (
+                    fragment_result_key not in st.session_state
+                )
+
+                if not _is_first_time_calling_fragment:
+                    previous_output = st.session_state.get(fragment_result_key, None)
+
+                # save the output for next re-run
+                st.session_state[fragment_result_key] = output
+
+                # reruns should only occur after the first fragment run (otherwise you're in a loop anyways)
+                if not _is_first_time_calling_fragment:
+                    _do_rerun_strategy(
+                        previous_output, output, on_return_strategy=on_return_strategy
+                    )
+
+                # this is odd, but the output is returned on re-run, so this is actually getting the output from before the
+                # triggered rerun
+                return_value = (
+                    output if _is_first_time_calling_fragment else previous_output
+                )
+                return return_value
+
+            return _wrapped_fragment(*args, **kwargs)
+
+        return _frame_intercepting_wrapper
+
+    if fn is None:
+        return _closed_parameters_decorator
+    else:
+        # NOTE: all other parameters will be their defaults
+        return _closed_parameters_decorator(fn)
+
+
+def _do_rerun_strategy(
+    old_value: R, output: R, *, on_return_strategy: SmartFragmentReturnStrategies
+):
+    if on_return_strategy == "rerun_not_on_none" and output is not None:
+        st.rerun(scope="app")
+
+    if on_return_strategy == "rerun_on_change" and output != old_value:
+        st.rerun(scope="app")
---
+++ try/try_smart_frags_2.py
@@ -0,0 +1,35 @@
+import datetime as dt
+
+import streamlit as st
+
+import src as stu
+from src import smart_fragment
+
+
+@smart_fragment
+def x():
+    """testing"""
+    st.write("a")
+    "# hello"
+    "test"
+
+
+@smart_fragment(on_return_strategy="no_rerun")
+def y():
+    val = stu.widgets.toggle("a")
+    stu.widgets.toggle("b")
+
+    return val
+
+
+x()
+x()
+res = y()
+save_btn = st.button("save")
+
+st.write(dt.datetime.now())
+
+st.session_state
+
+if save_btn:
+    st.write(res)
